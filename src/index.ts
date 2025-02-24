95c21aa46e3d37a96a7f71786c2c69ea30bd54a6
ahmedjk34 <111058820+ahmedjk34@users.noreply.github.com>
2025-02-24 16:33:33 +0200

    Created API paths & redirected them to the api.ts file

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,41 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+import { getAllQuotes, getQuotesByAuthor, getQuotesByBook, getQuotesByText, getRandomQuote, getTodaysQuote } from './routes/api';
+
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		const url = new URL(request.url);
+		const path = url.pathname;
+		const searchParams = url.searchParams;
+
+		let responseQuote;
+
+		// Handle the request based on URL path
+		if (path === '/quotes') {
+			if (searchParams.has('text')) {
+				responseQuote = getQuotesByText(searchParams.get('text') ?? '');
+			} else if (searchParams.has('author')) {
+				responseQuote = getQuotesByAuthor(searchParams.get('author') ?? '');
+			} else if (searchParams.has('book')) {
+				responseQuote = getQuotesByBook(searchParams.get('book') ?? '');
+			} else {
+				responseQuote = getAllQuotes();
+			}
+		} else if (path === '/quote/today') {
+			responseQuote = getTodaysQuote();
+		} else if (path === '/quote/random') {
+			responseQuote = getRandomQuote();
+		} else {
+			return new Response('Not Found', { status: 404 });
+		}
+
+		// Return the quote as JSON or an error message if not found
+		console.log();
+		if (responseQuote) {
+			return new Response(JSON.stringify(responseQuote), { headers: { 'Content-Type': 'application/json' } });
+		} else {
+			return new Response('Quote not found', { status: 404 });
+		}
 	},
 } satisfies ExportedHandler<Env>;
